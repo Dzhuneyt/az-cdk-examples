6534bd148a0c09ca18ba6f85fbf1f239a2ca824e
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-09-04 12:19:33 +1000

    Add notification to website pipeline

--- src/az-cdk/index-website-pip.ts
+++ src/az-cdk/index-website-pip.ts
@@ -17,6 +17,7 @@ new WebsitePipelineStack(app, stackName, {
   githubUser: config.githubUser,
   websiteBucketName: `${envars.WEBSITE_DOMAIN}-website`,
   cloudfrontDistributionId: envars.WEBSITE_CLOUDFRONT_ID,
+  notificationEmail: envars.PIPELINE_NOTIFICATION_EMAIL,
   assetsDir: 'public',
   githubSecret,
   envars,
