bd0670335eddb060c11e9c6e042b70656a835cbc
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-11-09 20:24:03 +1000

    Remove husky

---
+++ public-app/build/precache-manifest.d18730379cdca6e36a7227a69f4043f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1720adc4aa03af01908002041c67340",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0e995198050672ae84c",
+    "url": "/static/css/2.65b39531.chunk.css"
+  },
+  {
+    "revision": "c0e995198050672ae84c",
+    "url": "/static/js/2.8a6bcd46.chunk.js"
+  },
+  {
+    "revision": "ae6a455b34fb31c88d1d",
+    "url": "/static/js/main.47f93f18.chunk.js"
+  },
+  {
+    "revision": "554c8fb1ee9d82a1107d",
+    "url": "/static/js/runtime-main.3e1776e7.js"
+  }
+]);
