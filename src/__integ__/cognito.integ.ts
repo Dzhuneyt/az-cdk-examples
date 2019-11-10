d7dcac15a62e0e6ea9916c4527d0b3530633fc19
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-11-11 07:52:53 +1000

    Improve postConfirmation and integ tests

--- src/__integ__/cognito.integ.ts
+++ src/__integ__/cognito.integ.ts
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 import Amplify from '@aws-amplify/core';
 import Auth from '@aws-amplify/auth';
 import { v4 } from 'uuid';
-import { deleteUser, findUser } from '@cpmech/az-cognito';
+import { deleteUser, findUser, decodePayload } from '@cpmech/az-cognito';
 import {
   deleteEmail,
   receiveEmail,
@@ -35,14 +35,9 @@ Amplify.configure({
 const password = '123paSSword$';
 let email: string = '';
 let username: string = '';
-let emailReceiptHandle: string = '';
 
 const cleanUp = async () => {
   try {
-    if (emailReceiptHandle) {
-      await deleteEmail(emailReceiptHandle, envars.EMAILS_QUEUE_URL);
-      console.log('... email deleted successfully ...');
-    }
     if (username) {
       await deleteUser(username, envars.USER_POOL_ID);
       console.log('... user deleted successfully ...');
@@ -55,7 +50,6 @@ const cleanUp = async () => {
 beforeEach(() => {
   email = `tester+${v4()}@azcdk.xyz`;
   username = '';
-  emailReceiptHandle = '';
 });
 
 afterEach(async () => {
@@ -73,32 +67,47 @@ describe('cognito', () => {
     console.log('>>> username = ', username);
     expect(res.userConfirmed).toBe(false);
 
-    sleep(1000);
     console.log('2: receiving email');
+    await sleep(2000);
     const r = await receiveEmail(email, envars.EMAILS_QUEUE_URL);
-    emailReceiptHandle = r.receiptHandle;
+    const emailReceiptHandle = r.receiptHandle;
 
     console.log('3: extracting code from email');
     const code = await extractCodeFromEmail(r.content);
     console.log('>>> code = ', code);
 
-    console.log('4: confirming email with given code');
+    console.log('4: deleting email');
+    await deleteEmail(emailReceiptHandle, envars.EMAILS_QUEUE_URL);
+
+    console.log('5: confirming email with given code');
     await Auth.confirmSignUp(username, code);
     const userJustConfirmed = await findUser(email, envars.USER_POOL_ID);
     expect(userJustConfirmed.Data.email).toBe(email);
     expect(userJustConfirmed.Data.email_verified).toBe('true');
 
-    console.log('5: signing in');
+    console.log('6: signing in');
     const user = await Auth.signIn({ username, password });
     expect(user.attributes.email_verified).toBe(true);
 
-    sleep(1000);
-    console.log('6: receive confirmation email');
+    console.log('7: receiving confirmation email');
+    await sleep(2000);
     const rc = await receiveEmail(email, envars.EMAILS_QUEUE_URL);
     const sm = await extractSubjectAndMessage(rc.content);
     expect(sm).toEqual({
       subject: 'Welcome to AZCDK!',
-      message: 'Your account has been created successfully\n',
+      message: `Hello,
+
+Your account has been created successfully.
+
+Enjoy!
+
+
+`,
     });
+
+    console.log('8: checking group');
+    const payload = await decodePayload(user);
+    console.log(payload);
+    expect(payload['cognito:groups']).toEqual(['travellers']);
   });
 });
--- src/lambda/cognitoPostConfirm.ts
+++ src/lambda/cognitoPostConfirm.ts
@@ -2,50 +2,87 @@ import AWS from 'aws-sdk';
 import { ILambdaCognito, IEventCognito } from '@cpmech/az-lambda';
 import { sendEmail } from '@cpmech/az-senqs';
 
+const GROUP_NAME = 'travellers';
+
 export const handler: ILambdaCognito = async (event: IEventCognito): Promise<any> => {
-  const { email, userName } = event.request.userAttributes;
+  const { userName } = event;
+  const { email, name } = event.request.userAttributes;
   console.log('>>> event = ', event);
 
+  // check
+  if (!userName) {
+    throw new Error('cannot get userName from event data');
+  }
+  if (!email) {
+    throw new Error('cannot get email from event data');
+  }
+
+  // status
+  const status = event.request.userAttributes['cognito:user_status'];
+  let info = '';
+  if (status === 'EXTERNAL_PROVIDER') {
+    const provider = userName.split('_')[0];
+    info = `(account created with ${provider} credentials)`;
+    console.log(info);
+  }
+
   // send confirmation email
-  if (email) {
-    try {
-      await sendEmail(
-        'tester@azcdk.xyz',
-        [email],
-        `Welcome to AZCDK!`,
-        `Your account has been created successfully`,
-      );
-    } catch (error) {
-      console.log(error.message || JSON.stringify(error));
-    }
+  console.log('... sending confirmation email ...');
+  try {
+    await sendEmail(
+      'tester@azcdk.xyz',
+      [email],
+      `Welcome to AZCDK!`,
+      `Hello${name ? ' ' + name : ''},
+
+Your account has been created successfully.
+
+Enjoy!
+
+${info || ''}
+`,
+    );
+  } catch (error) {
+    console.log(error.message || JSON.stringify(error));
   }
 
   // add user to group
-  if (userName) {
-    // cognito
-    const cognito = new AWS.CognitoIdentityServiceProvider({
-      apiVersion: '2016-04-18',
-    });
+  // cognito
+  const cognito = new AWS.CognitoIdentityServiceProvider({
+    // apiVersion: '2016-04-18',
+  });
 
-    // create group if inexistent
-    const groupParams = {
-      GroupName: 'customers',
-      UserPoolId: event.userPoolId,
-    };
+  // create group if inexistent
+  console.log('... creating group ...');
+  const groupParams = {
+    GroupName: GROUP_NAME,
+    UserPoolId: event.userPoolId,
+  };
+  try {
+    await cognito.getGroup(groupParams).promise();
+  } catch (error) {
+    /* ignore error: group does not exist */
     try {
-      await cognito.getGroup(groupParams).promise();
-    } catch (error) {
       await cognito.createGroup(groupParams).promise();
+    } catch (err) {
+      console.log('... group creation failed:', err);
     }
-
-    // add to group
-    const addUserParams = {
-      GroupName: 'customers',
-      UserPoolId: event.userPoolId,
-      Username: userName,
-    };
-    await cognito.adminAddUserToGroup(addUserParams).promise();
   }
+
+  // add to group
+  console.log('... adding user to group ...');
+  const addUserParams = {
+    GroupName: GROUP_NAME,
+    UserPoolId: event.userPoolId,
+    Username: userName,
+  };
+  try {
+    await cognito.adminAddUserToGroup(addUserParams).promise();
+  } catch (error) {
+    console.log('... failure when adding user to group:', error);
+  }
+
+  // add entry to database
 
   // response
   return event;
