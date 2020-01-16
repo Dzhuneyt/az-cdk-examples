0237e5b7fe24b406248995ef59ccd3c509fa48d6
Dorival Pedroso <dorival.pedroso@gmail.com>
2020-01-16 17:51:47 +1000

    Add more options to Dynamo table

--- az-cdk/index-dynamo.ts
+++ az-cdk/index-dynamo.ts
@@ -1,4 +1,5 @@
 import { App, Stack } from '@aws-cdk/core';
+import { AttributeType, ProjectionType } from '@aws-cdk/aws-dynamodb';
 import { DynamoConstruct } from '@cpmech/az-cdk';
 import { cfg } from './envars';
 
@@ -10,13 +11,22 @@ new DynamoConstruct(stack, 'Dynamo', {
   dynamoTables: [
     {
       name: cfg.tableParams,
-      partitionKey: 'category',
-      sortKey: 'key',
+      partitionKey: 'paramId',
+      sortKey: 'category',
+      onDemand: true,
     },
     {
       name: cfg.tableUsers,
       partitionKey: 'userId',
       sortKey: 'aspect',
+      onDemand: true,
+      gsi: {
+        indexName: 'email2access',
+        partitionKey: { name: 'access', type: AttributeType.STRING },
+        sortKey: { name: 'email', type: AttributeType.STRING },
+        nonKeyAttributes: ['phone', 'accountStatus'],
+        projectionType: ProjectionType.INCLUDE,
+      },
     },
   ],
 });
